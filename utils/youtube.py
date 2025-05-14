5aa52359422a122312a3deea5b05c9ebf26d6175
tom1022 <tom1022.v2@gmail.com>
2025-05-14 10:29:55 +0900

    Fix: Disable automatic browser opening during YouTube credentials flow

--- utils/youtube.py
+++ utils/youtube.py
@@ -17,7 +17,8 @@ def get_youtube_credentials():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server(port=0, open_browser=False)
+
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
     return creds
